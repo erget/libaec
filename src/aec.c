833c349cf1c236c13eb31bafcb868139401b4bbb
Mathis Rosenhauer <rosenhauer@dkrz.de>
2012-09-16 17:05:06 +0200

    Merged encode and decode into aec

--- src/aec.c
+++ src/aec.c
@@ -1,7 +1,7 @@
 #include <ctype.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <inttypes.h>
 #include <string.h>
 #include <getopt.h>
@@ -14,11 +14,12 @@ int main(int argc, char *argv[])
     ae_stream strm;
     uint8_t *in;
     uint8_t *out;
-    int chunk, c, total_out, status;
+    int chunk, total_out, status, c;
     int input_avail, output_avail;
     char *outfn, *infn, *ext;
     FILE *infp, *outfp;
     int cflag = 0;
+    int dflag = 0;
 
     chunk = CHUNK;
     strm.bit_per_sample = 8;
@@ -27,9 +28,12 @@ int main(int argc, char *argv[])
     strm.flags = AE_DATA_PREPROCESS;
     opterr = 0;
 
-    while ((c = getopt (argc, argv, "3Mscb:B:J:R:")) != -1)
+    while ((c = getopt (argc, argv, "d3Mscb:B:R:J:")) != -1)
         switch (c)
         {
+        case 'd':
+            dflag = 1;
+            break;
         case 'b':
             chunk = atoi(optarg);
             break;
@@ -90,10 +94,23 @@ int main(int argc, char *argv[])
         chunk *= 2;
     }
 
-    in = (uint8_t *)malloc(chunk);
     out = (uint8_t *)malloc(chunk);
+    in = (uint8_t *)malloc(chunk);
+
+
     if (in == NULL || out == NULL)
-        return 1;
+        exit(-1);
+
+    total_out = 0;
+    strm.avail_in = 0;
+    strm.avail_out = chunk;
+    strm.next_out = out;
+
+    input_avail = 1;
+    output_avail = 1;
+
+    if ((infp = fopen(infn, "r")) == NULL)
+        exit(-1);
 
     if (cflag)
     {
@@ -101,46 +118,55 @@ int main(int argc, char *argv[])
     }
     else
     {
-        outfn = malloc(strlen(infn));
+        outfn = malloc(strlen(infn) + 4);
         if (outfn == NULL)
             exit(-1);
 
-        if ((ext = strstr(infn, ".aee")) == NULL)
+        if (dflag)
         {
-            fprintf(stderr, "Error: input file needs to end with .aee\n");
-            exit(-1);
+            if ((ext = strstr(infn, ".aee")) == NULL)
+            {
+                fprintf(stderr, "Error: input file needs to end with .aee\n");
+                exit(-1);
+            }
+            strncpy(outfn, infn, ext - infn);
         }
-        strncpy(outfn, infn, ext - infn);
+        else
+        {
+            sprintf(outfn, "%s.aee", infn);
+        }
 
         if ((outfp = fopen(outfn, "w")) == NULL)
             exit(-1);
     }
-    if ((infp = fopen(infn, "r")) == NULL)
-        exit(-1);
 
-    if (ae_decode_init(&strm) != AE_OK)
-        return 1;
-
-    total_out = 0;
-    strm.avail_in = 0;
-    strm.avail_out = chunk;
-    strm.next_out = (uint8_t *)out;
-
-    input_avail = 1;
-    output_avail = 1;
+    if (dflag)
+    {
+        if (ae_decode_init(&strm) != AE_OK)
+            return 1;
+    }
+    else
+    {
+        if (ae_encode_init(&strm) != AE_OK)
+            return 1;
+    }
 
     while(input_avail || output_avail)
     {
-        if (strm.avail_in == 0)
+        if (strm.avail_in == 0 && input_avail)
         {
             strm.avail_in = fread(in, 1, chunk, infp);
             if (strm.avail_in != chunk)
                 input_avail = 0;
-
-            strm.next_in = in;
+            strm.next_in = (uint8_t *)in;
         }
 
-        if ((status = ae_decode(&strm, AE_NO_FLUSH)) != AE_OK)
+        if (dflag)
+            status = ae_decode(&strm, AE_NO_FLUSH);
+        else
+            status = ae_encode(&strm, AE_NO_FLUSH);
+
+        if (status != AE_OK)
         {
             fprintf(stderr, "error is %i\n", status);
             return 1;
@@ -151,7 +177,7 @@ int main(int argc, char *argv[])
             fwrite(out, strm.total_out - total_out, 1, outfp);
             total_out = strm.total_out;
             output_avail = 1;
-            strm.next_out = (uint8_t *)out;
+            strm.next_out = out;
             strm.avail_out = chunk;
         }
         else
@@ -161,7 +187,26 @@ int main(int argc, char *argv[])
 
     }
 
-    ae_decode_end(&strm);
+    if (dflag)
+    {
+        ae_decode_end(&strm);
+    }
+    else
+    {
+        if ((status = ae_encode(&strm, AE_FLUSH)) != AE_OK)
+        {
+            fprintf(stderr, "error is %i\n", status);
+            return 1;
+        }
+
+        if (strm.total_out - total_out > 0)
+        {
+            fwrite(out, strm.total_out - total_out, 1, outfp);
+        }
+
+        ae_encode_end(&strm);
+    }
+
     fclose(infp);
     fclose(outfp);
     free(in);
